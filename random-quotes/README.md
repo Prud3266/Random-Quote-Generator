c64b2c5afdd44028e0b43919148beecd85621f6f
Jeremiah <103808024+Prud3266@users.noreply.github.com>
2022-11-18 05:05:13 +0100

    Add files via upload
    A simple motivational quote generator.

---
+++ random-quotes/README.md
@@ -0,0 +1,4 @@
+# Random Quotes
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/prud3266/pen/vYrezRR](https://codepen.io/prud3266/pen/vYrezRR).
+
---
+++ random-quotes/src/script.js
@@ -0,0 +1,24 @@
+const Quote = document.getElementById('quote');
+const By = document.getElementById('by');
+
+const getquote = async () => {
+  
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cba7954da9msh471cabcfe861514p1362f1jsn48155097017b',
+		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+	}
+};
+
+fetch('https://famous-quotes4.p.rapidapi.com/random?category=all&count=2', options)
+	.then(response => response.json())
+	.then(response => {
+  Quote.innerText = response[0].text; 
+  By.innerText = `- ${response[0].author}`
+  })
+	.catch(err => console.error(err));
+}
+
+const getQuote = () => getquote();
+ 
